a4081e94bd294c9725c9f89f7a6885f3c5521f8c
root <root@fuel.domain.tld>
2016-09-04 11:00:12 +0000

    Use defined type

--- updpkgfuel-library/pkg_upgrade.pp
+++ updpkgfuel-library/pkg_upgrade.pp
@@ -1,41 +1,34 @@
-#
-class osnailyfacter::upgrade::pkg_upgrade {
-  # hardcode with retries and sleeps for resolving lock issue
-  # should be rewritten
-  exec { 'do_upgrade':
-    command     => 'apt-get dist-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"',
-    environment => [ 'DEBIAN_FRONTEND=noninteractive' ],
-    path        => ['/usr/bin', '/usr/local/sbin', '/usr/sbin', '/sbin', '/bin' ],
-    timeout     => 1700,
-    try_sleep   => 10,
-    tries       => 5,
-    logoutput   => true,
-  }
-
+# Update packages (Ubuntu case)                                                     
+class osnailyfacter::upgrade::pkg_upgrade {           
+                                                      
+  osnailyfacter::upgrade::pkgs{'do_upgrade':  }       
+                                                      
   $corosync_roles = hiera('corosync_roles', ['primary-controller', 'controller'])
-  if roles_include($corosync_roles) {
+  if roles_include($corosync_roles) {                 
     $content_policy = "#!/bin/bash\n[[ \"\$1\" == \"pacemaker\" ]] && exit 101\n"
-    $policyrc_file  = '/usr/sbin/policy-rc.d'
-
-    ensure_resource('file', 'create-policy-rc.d', {
-      ensure  => present,
-      path    => $policyrc_file,
-      content => $content_policy,
-      mode    => '0755',
-      owner   => 'root',
-      group   => 'root',
-      before  => Exec['do_upgrade']
-    })
-
-    exec { 'remove_policy':
-      command => "rm -rf ${policyrc_file}",
-      path    => '/bin',
-      require => Exec['do_upgrade'],
-    }
-
-    ensure_resource('service', 'pacemaker', {
-      ensure  => running,
-      require => Exec['remove_policy']
-    })
-  }
+    $policyrc_file  = '/usr/sbin/policy-rc.d'         
+                                                      
+    ensure_resource('file', 'create-policy-rc.d', {   
+      ensure  => present,                             
+      path    => $policyrc_file,                      
+      content => $content_policy,                     
+      mode    => '0755',                              
+      owner   => 'root',                              
+      group   => 'root',                              
+      before  => Osnailyfacter::Upgrade::Pkgs['do_upgrade'],
+    })                                                
+                                                      
+    exec { 'remove_policy':                           
+      command => "rm -rf ${policyrc_file}",           
+      path    => '/bin',                              
+      require => Osnailyfacter::Upgrade::Pkgs['do_upgrade'],
+    }                                                 
+                                                      
+    ensure_resource('service', 'pacemaker', {         
+      ensure  => running,                             
+      require => Exec['remove_policy']                
+    })                                                
+  }                                                   
 }
+
+
